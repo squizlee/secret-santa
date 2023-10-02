8f01ff286e616c0e652aa8de14053f00b081cc8e
squizlee <ethanscavia777@gmail.com>
2023-10-02 14:50:45 +1100

    initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Secret Santa 🎅🤫
+
+Easily prepare secret santa from a list of names
+
+## Project Structure
+- /client: sveltekit app front end
+- /server: c# backend logic
+- /: cdk application for cloudformation deployments to AWS
+
+## Contributions Welcome
---
+++ client/src/routes/+page.svelte
@@ -0,0 +1,129 @@
+<script lang="ts">
+	import { fade } from 'svelte/transition';
+	const test_input =
+		'John Smith john.smith@example.com, Mary Johnson mary.johnson@example.com, James Brown james.brown@example.com, Emily Davis emily.davis@example.com, Michael Wilson michael.wilson@example.com';
+
+	interface Name {
+		name: string;
+		email: string;
+	}
+
+	let name_input: string;
+	let names: Name[];
+	let send_emails: boolean = true;
+	let create_visual: boolean = true;
+	let display_second_form: boolean = false;
+
+	const name_parser = (names_input: string) => {
+		const names_array: Name[] = [];
+		names_input.split(/,|\n/).forEach((data_point: string) => {
+			const re = /^(?<name>.+?)\s+(?<email>\S+?@\S+)$/;
+			const search_result = re.exec(data_point);
+			if (!search_result) {
+				console.error('error: failed to retrieve details from data point', data_point);
+				return;
+			}
+			console.log(search_result);
+			names_array.push({ name: search_result?.groups!.name, email: search_result?.groups!.email });
+		});
+		names = names_array;
+		display_second_form = true; // display second form
+	};
+</script>
+
+<svelte:head>
+	<title>Secret Santa Creator</title>
+	<meta name="secret santa" content="Secret Santa Creator" />
+</svelte:head>
+
+<header>
+	<h1>Secret <br /> Santa 🤫🎅</h1>
+</header>
+
+<section class="container">
+	<section id="form">
+		<form action="#" on:submit|preventDefault={() => name_parser(name_input)}>
+			<label for="names">Names</label>
+			<textarea bind:value={name_input} id="names" name="names" rows="6" cols="80" />
+			<input type="submit" value="package" />
+		</form>
+	</section>
+</section>
+
+{#if display_second_form}
+	<section in:fade class="finalise">
+		<p>Found {names.length} names</p>
+		<div>
+			<label for="email">Send emails</label>
+			<input bind:value={send_emails} type="checkbox" name="email" id="email" checked />
+		</div>
+		<div>
+			<label for="image">Create Gift Chain Image</label>
+			<input bind:value={create_visual} type="checkbox" name="image" id="image" checked />
+		</div>
+		<input type="submit" value="send!" />
+	</section>
+{/if}
+
+<p>
+	John Smith john.smith@example.com, Mary Johnson mary.johnson@example.com, James Brown
+	james.brown@example.com, Emily Davis emily.davis@example.com, Michael Wilson
+	michael.wilson@example.com
+</p>
+
+<!-- TODO ADD GitHub Repo link in footer tag-->
+<!-- <footer>
+	<a href="#">TODO: GH LINK</a>
+</footer> -->
+
+<style>
+	h1 {
+		margin: 0;
+		font-size: 6rem;
+	}
+
+	header {
+		background-image: var(--background-image-url);
+		background-repeat: no-repeat;
+		color: red;
+	}
+
+	.finalise {
+		background-color: var(--kelly-green);
+		margin: 0;
+	}
+
+	form {
+		min-width: 60vw;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		padding: 1rem;
+		gap: 10px;
+		margin: 10px;
+	}
+
+	textarea {
+		border: 2px solid black;
+	}
+
+	form > label[for='names'] {
+		font-size: 2.5rem;
+		align-self: flex-start;
+	}
+
+	.finalise > p {
+		font-size: 2.5rem;
+	}
+
+	form > *,
+	.finalise > * {
+		font-family: 'Vollkorn', 'Times New Roman', Times, serif;
+	}
+
+	.container {
+		display: flex;
+		background-color: rgb(255, 62, 62);
+		border: 2px solid black;
+	}
+</style>
